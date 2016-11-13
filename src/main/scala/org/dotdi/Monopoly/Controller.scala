d4927ab6d552a6afcdd78f5597c703ab4f851eec
Dotan Di Castro <dot@yahoo-inc.com>
2016-11-13 23:27:32 +0200

    update

--- src/main/scala/org/dotdi/Monopoly/Controller.scala
+++ src/main/scala/org/dotdi/Monopoly/Controller.scala
@@ -1,6 +1,6 @@
 package org.dotdi.Monopoly
 
-case class Controller(numPlayers:Int) {
-  
+case class Controller(numPlayers:Int, numCells:Int = 40) {
+  val cellState = Array.fill[CellState](numCells)(CellState())
   
 }
